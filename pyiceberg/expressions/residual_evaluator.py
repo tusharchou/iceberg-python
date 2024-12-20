8b59e57faa3ce90f1ce70a1a1fa1d7b58a7b5213
Tushar Choudhary <151359025+tusharchou@users.noreply.github.com>
2024-12-20 16:58:59 +0530

    test_identity_transform_residual

--- pyiceberg/expressions/residual_evaluator.py
+++ pyiceberg/expressions/residual_evaluator.py
@@ -32,13 +32,11 @@ class ResidualVisitor(BoundBooleanExpressionVisitor[BooleanExpression], ABC):
         self.case_sensitive = case_sensitive
         self.expr = expr
 
-    # def project(self, expr: BooleanExpression) -> BooleanExpression:
-    #     #  projections assume that there are no NOT nodes in the expression tree. to ensure that this
-    #     #  is the case, the expression is rewritten to push all NOT nodes down to the expression
-    #     #  leaf nodes.
-    #     #  this is necessary to ensure that the default expression returned when a predicate can't be
-    #     #  projected is correct.
-    #     return visit(bind(self.schema, rewrite_not(expr), self.case_sensitive), self)
+
+    def eval(self, partition_data: Record):
+        self.struct = partition_data
+        return visit(self.expr, visitor=self)
+
 
     def visit_true(self) -> BooleanExpression:
         return AlwaysTrue()
@@ -47,43 +45,27 @@ class ResidualVisitor(BoundBooleanExpressionVisitor[BooleanExpression], ABC):
         return AlwaysFalse()
 
     def visit_not(self, child_result: BooleanExpression) -> BooleanExpression:
-        raise ValueError(f"Cannot project not expression, should be rewritten: {child_result}")
-
+        return Not(child_result)
     def visit_and(self, left_result: BooleanExpression, right_result: BooleanExpression) -> BooleanExpression:
         return And(left_result, right_result)
 
     def visit_or(self, left_result: BooleanExpression, right_result: BooleanExpression) -> BooleanExpression:
         return Or(left_result, right_result)
 
-    def visit_unbound_predicate(self, predicate: UnboundPredicate[L]) -> BooleanExpression:
-        bound = predicate.bind(self.schema, case_sensitive=True)
 
-        if isinstance(bound, BoundPredicate):
-            bound_residual = visit(bound,self)
-            bound_residual = self.visit_bound_predicate(predicate=bound)
-            # if isinstance(bound_residual, BooleanExpression):
-            #     return predicate
+    def visit_is_null(self, term: BoundTerm[L]) -> bool:
+        return term.eval(self.struct) is None
 
-            return bound_residual
+    def visit_not_null(self, term: BoundTerm[L]) -> bool:
+        return term.eval(self.struct) is not None
 
+    def visit_is_nan(self, term: BoundTerm[L]) -> bool:
+        val = term.eval(self.struct)
+        return val != val
 
-        # return bound
-
-    # def visitor(self):
-    #     if not hasattr(self.thread_local_data, "visitors"):
-    #         self.thread_local_data.visitors = ResidualVisitor()
-    #
-    #     return self.thread_local_data.visitors
-
-    def visit_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        if term.eval(self.struct) == literal.value:
-            return self.visit_true()
-        else:
-            return self.visit_false()
-
-    def visit_not_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        return term.eval(self.struct) != literal.value
-
+    def visit_not_nan(self, term: BoundTerm[L]) -> bool:
+        val = term.eval(self.struct)
+        return val == val
 
     def visit_less_than(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
         if term.eval(self.struct) < literal.value:
@@ -91,8 +73,8 @@ class ResidualVisitor(BoundBooleanExpressionVisitor[BooleanExpression], ABC):
         else:
             return self.visit_false()
 
-    def visit_greater_than_or_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        if term.eval(self.struct) >= literal.value:
+    def visit_less_than_or_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
+        if term.eval(self.struct) <= literal.value:
             return self.visit_true()
         else:
             return self.visit_false()
@@ -103,48 +85,43 @@ class ResidualVisitor(BoundBooleanExpressionVisitor[BooleanExpression], ABC):
         else:
             return self.visit_false()
 
-    def visit_less_than(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        if term.eval(self.struct) < literal.value:
+    def visit_greater_than_or_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
+        if term.eval(self.struct) >= literal.value:
             return self.visit_true()
         else:
             return self.visit_false()
-    def visit_less_than_or_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        if term.eval(self.struct) <= literal.value:
+
+
+
+
+    def visit_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
+        if term.eval(self.struct) == literal.value:
             return self.visit_true()
         else:
             return self.visit_false()
+
+    def visit_not_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
+        if term.eval(self.struct) != literal.value:
+            return self.visit_true()
+        else:
+            return self.visit_false()
+
+
+    def visit_in(self, term: BoundTerm[L], literals: Set[L]) -> bool:
+        if term.eval(self.struct) in literals:
+            return self.visit_true()
+        else:
+            return self.visit_false()
+    def visit_not_in(self, term: BoundTerm[L], literals: Set[L]) -> bool:
+        return term.eval(self.struct) not in literals
+
+
     def visit_starts_with(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
         eval_res = term.eval(self.struct)
         return eval_res is not None and str(eval_res).startswith(str(literal.value))
 
     def visit_not_starts_with(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
         return not self.visit_starts_with(term, literal)
-
-
-    def visit_in(self, term: BoundTerm[L], literals: Set[L]) -> bool:
-        return term.eval(self.struct) in literals
-
-    def visit_not_in(self, term: BoundTerm[L], literals: Set[L]) -> bool:
-        return term.eval(self.struct) not in literals
-
-    def visit_is_nan(self, term: BoundTerm[L]) -> bool:
-        val = term.eval(self.struct)
-        return val != val
-
-    def visit_not_nan(self, term: BoundTerm[L]) -> bool:
-        val = term.eval(self.struct)
-        return val == val
-
-    def visit_is_null(self, term: BoundTerm[L]) -> bool:
-        return term.eval(self.struct) is None
-
-    def visit_not_null(self, term: BoundTerm[L]) -> bool:
-        return term.eval(self.struct) is not None
-
-
-    def eval(self, partition_data: Record):
-        self.struct = partition_data
-        return visit(self.expr, visitor=self)
 
     def visit_bound_predicate(self, predicate: BoundPredicate[Any]) -> List[str]:
         """
@@ -155,14 +132,15 @@ class ResidualVisitor(BoundBooleanExpressionVisitor[BooleanExpression], ABC):
         if parts == []:
             return predicate
 
+        from pyiceberg.types import StructType
+        def struct_to_schema(struct: StructType) -> Schema:
+            return Schema(*[f for f in struct.fields])
+
         for part in parts:
             strict_projection = part.transform.strict_project(part.name, predicate)
             strict_result = None
             # return strict_projection
             if strict_projection is not None:
-                from pyiceberg.types import StructType
-                def struct_to_schema(struct: StructType) -> Schema:
-                    return Schema(*[f for f in struct.fields])
 
                 bound = strict_projection.bind(struct_to_schema(self.spec.partition_type(self.schema)))
                 from pyiceberg.expressions import AlwaysFalse
@@ -193,6 +171,20 @@ class ResidualVisitor(BoundBooleanExpressionVisitor[BooleanExpression], ABC):
 
         # return predicate
 
+    def visit_unbound_predicate(self, predicate: UnboundPredicate[L]) -> BooleanExpression:
+        bound = predicate.bind(self.schema, case_sensitive=True)
+
+        if isinstance(bound, BoundPredicate):
+            bound_residual = visit(bound,self)
+            bound_residual = self.visit_bound_predicate(predicate=bound)
+            # if isinstance(bound_residual, BooleanExpression):
+            #     return predicate
+
+            return bound_residual
+
+
+        # return bound
+
 
 
 
@@ -201,11 +193,24 @@ class ResidualEvaluator(ResidualVisitor):
     def residual_for(self, partition_data):
         return self.eval(partition_data)
 
+class UnpartitionedResidualEvaluator(ResidualEvaluator):
+
+    def __init__(self, schema: Schema,expr: BooleanExpression):
+        from pyiceberg.partitioning import UNPARTITIONED_PARTITION_SPEC
+        super().__init__(schema=schema, spec=UNPARTITIONED_PARTITION_SPEC, expr=expr, case_sensitive=False)
+        self.expr = expr
+
+    def residual_for(self, partition_data):
+        return self.expr
+
+
 def residual_evaluator_of(
         spec: PartitionSpec,
         expr: BooleanExpression,
         case_sensitive: bool,
         schema: Schema
 ) -> ResidualEvaluator:
-    return ResidualEvaluator(spec=spec, expr=expr, schema=schema, case_sensitive=case_sensitive)
-
+    if len(spec.fields) != 0:
+        return ResidualEvaluator(spec=spec, expr=expr, schema=schema, case_sensitive=case_sensitive)
+    else:
+        return UnpartitionedResidualEvaluator(schema=schema,expr=expr)
--- tests/expressions/test_residual_evaluator.py
+++ tests/expressions/test_residual_evaluator.py
@@ -7,19 +7,31 @@ from pyiceberg.expressions import (
     And,
     Or,
     GreaterThan,
+    GreaterThanOrEqual,
     UnboundPredicate,
     BoundPredicate,
     BoundReference,
     BooleanExpression,
     BoundLessThan,
-    BoundGreaterThan
+    BoundGreaterThan,
+    NotNull,
+    IsNull,
+    In,
+    NotIn,
+    NotNaN,
+    IsNaN,
+    StartsWith,
+    NotStartsWith
 )
 from pyiceberg.expressions.residual_evaluator import residual_evaluator_of
 from pyiceberg.partitioning import PartitionField, PartitionSpec
 from pyiceberg.schema import Schema
-from pyiceberg.transforms import IdentityTransform
+from pyiceberg.transforms import IdentityTransform, DayTransform
 from pyiceberg.typedef import Record
-from pyiceberg.types import IntegerType, NestedField
+from pyiceberg.types import IntegerType, NestedField, StringType, TimestampType
+from pyiceberg.utils.datetime import timestamp_to_micros
+from pyiceberg.expressions.literals import literal
+
 
 
 def test_identity_transform_residual():
@@ -62,7 +74,111 @@ def test_identity_transform_residual():
 
     assert residual == AlwaysFalse()
 
+def test_case_insensitive_identity_transform_residuals():
+
+    schema = Schema(
+        NestedField(50, "dateint", IntegerType()),
+        NestedField(51, "hour", IntegerType())
+    )
+
+    spec = PartitionSpec(
+        PartitionField(50, 1050, IdentityTransform(), "dateint_part")
+    )
+
+    predicate = Or(
+        Or(
+            And(EqualTo("DATEINT", 20170815), LessThan("HOUR", 12)),
+            And(LessThan("dateint", 20170815), GreaterThan("dateint", 20170801))
+        ),
+        And(EqualTo("Dateint", 20170801), GreaterThan("hOUr", 11))
+    )
+    res_eval = residual_evaluator_of(spec=spec,expr=predicate, case_sensitive=True, schema=schema)
+
+
+    with pytest.raises(ValueError) as e:
+        residual = res_eval.residual_for(Record(dateint=20170815))
+    assert "Could not find field with name DATEINT, case_sensitive=True" in str(e.value)
+
+
+def test_unpartitioned_residuals():
+
+
+    expressions = [
+        AlwaysTrue(),
+        AlwaysFalse(),
+        LessThan("a", 5),
+        GreaterThanOrEqual("b", 16),
+        NotNull("c"),
+        IsNull("d"),
+        In("e",[1, 2, 3]),
+        NotIn("f", [1, 2, 3]),
+        NotNaN("g"),
+        IsNaN("h"),
+        StartsWith("data", "abcd"),
+        NotStartsWith("data", "abcd")
+    ]
+
+    schema = Schema(
+        NestedField(50, "dateint", IntegerType()),
+        NestedField(51, "hour", IntegerType()),
+        NestedField(52, "a", IntegerType())
+    )
+    for expr in expressions:
+        from pyiceberg.partitioning import UNPARTITIONED_PARTITION_SPEC
+        residual_evaluator = residual_evaluator_of(
+            UNPARTITIONED_PARTITION_SPEC, expr, True, schema=schema
+        )
+        assert residual_evaluator.residual_for(Record()) == expr
+
+def test_in():
+
+    schema = Schema(
+        NestedField(50, "dateint", IntegerType()),
+        NestedField(51, "hour", IntegerType())
+    )
+
+    spec = PartitionSpec(
+        PartitionField(50, 1050, IdentityTransform(), "dateint_part")
+    )
+
+    predicate = In("dateint", [20170815, 20170816, 20170817])
+
+    res_eval = residual_evaluator_of(spec=spec,expr=predicate, case_sensitive=True, schema=schema)
+
+    residual = res_eval.residual_for(Record(dateint=20170815))
+
+    assert residual == AlwaysTrue()
 
 
 
+def test_in_timestamp():
+
+    schema = Schema(
+        NestedField(50, "ts", TimestampType()),
+        NestedField(51, "hour", IntegerType())
+    )
+
+
+    spec = PartitionSpec(
+        PartitionField(50, 1000, DayTransform(), "ts_part")
+    )
+
+    date_20191201 = literal("2019-12-01T00:00:00").to(TimestampType()).value
+    date_20191202 = literal("2019-12-02T00:00:00").to(TimestampType()).value
+
+    day = DayTransform().transform(TimestampType())
+    # assert date_20191201 == True
+    ts_day = day(date_20191201)
+
+    assert ts_day == True
+
+    pred  = In("ts", [ date_20191202, date_20191201])
+
+    res_eval = residual_evaluator_of(spec=spec, expr=pred, case_sensitive=True, schema=schema)
+
+    residual = res_eval.residual_for(Record(ts_day))
+    assert residual == AlwaysTrue()
+
+    residual = res_eval.residual_for(Record(ts_day+3))
+    assert residual == AlwaysFalse()
 
